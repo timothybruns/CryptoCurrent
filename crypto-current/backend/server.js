37739fe25789f2c766f80b6a9243644c2915e1f3
Carson Chen <caiyongchen@live.com>
2017-12-21 15:22:25 -0500

    minor update to server js

--- crypto-current/backend/server.js
+++ crypto-current/backend/server.js
@@ -1,34 +1,43 @@
+// requires all modules
 require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const passport = require('passport');
 
+// modules for authentication, commented out in meantime.
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+// const passport = require('passport');
 
+// set express up
 const app = express();
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(cookieParser());
-app.use(
-  session({
-    key: process.env.SECRET_KEY,
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
-  }),
-  );
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static('public'));
 
+// modules for authentication, commented out in meantime.
+// app.use(cookieParser());
+// app.use(
+//   session({
+//     key: process.env.SECRET_KEY,
+//     secret: process.env.SECRET_KEY,
+//     resave: false,
+//     saveUninitialized: true,
+//   }),
+//   );
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(express.static('public'));
+
+// setup server root folder
 app.get('/', (req, res) => {
-  res.send('Hellow World!');
+  res.send('Hello World! Our group is AWESOME!');
 });
+
+// catching illegal routes
 app.use('*', (req,res) => {
   res.status(400).json({
     message: 'Not found!',
@@ -43,7 +52,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+// setup localhost POST based on env or 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
