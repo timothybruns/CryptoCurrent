db10bb92bee8aebd5703745823677b3731d69bb0
Carson Chen <caiyongchen@live.com>
2017-12-21 17:35:29 -0500

    completed partial blog controller - getall

--- crypto-current/backend/controllers/blogs-controller.js
+++ crypto-current/backend/controllers/blogs-controller.js
@@ -3,24 +3,23 @@ const blogsDB = require('../models/blogsDB');
 module.exports = {
 
   // get all blogs and set them in the res.local
-  index(req, res, next) => {
+  index(req, res, next) {
     blogsDB.findAll()
-    .then((blogs) => {
-      res.locals.blogs = blogs;
-      next();
-    })
-    .catch(err => next(err));
+      .then((blogs) => {
+        res.locals.blogs = blogs;
+        next();
+      })
+      .catch(err => next(err));
   },
 
   getOne(req, res, next) {
     blogsDB.findOne(req.params.id)
-    .then((post) => {
-      res.locals.post = post;
-      next();
-    })
-    .catch(err => next(err));
+      .then((post) => {
+        res.locals.post = post;
+        next();
+      })
+      .catch(err => next(err));
   },
-
 
 
 };
