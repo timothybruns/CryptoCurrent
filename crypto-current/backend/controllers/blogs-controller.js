da7d09b666de58da9d339ada199d480aa79b5066
Carson Chen <caiyongchen@live.com>
2017-12-21 17:10:11 -0500

    installed eslint

---
+++ crypto-current/backend/controllers/blogs-controller.js
@@ -0,0 +1,26 @@
+const blogsDB = require('../models/blogsDB');
+
+module.exports = {
+
+  // get all blogs and set them in the res.local
+  index(req, res, next) => {
+    blogsDB.findAll()
+    .then((blogs) => {
+      res.locals.blogs = blogs;
+      next();
+    })
+    .catch(err => next(err));
+  },
+
+  getOne(req, res, next) {
+    blogsDB.findOne(req.params.id)
+    .then((post) => {
+      res.locals.post = post;
+      next();
+    })
+    .catch(err => next(err));
+  },
+
+
+
+};
