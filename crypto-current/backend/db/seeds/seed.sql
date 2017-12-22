4cc72c406bcb90b38df5b05dc3ddf2b9aa9ccc08
Carson Chen <caiyongchen@live.com>
2017-12-22 13:55:13 -0500

    added few more dummy data

--- crypto-current/backend/db/seeds/seed.sql
+++ crypto-current/backend/db/seeds/seed.sql
@@ -1,8 +1,45 @@
 \c crypto_blog_dev
 
 INSERT INTO users(username, password)
-  VALUES ('Dan F`n Keenan', 'Non-ya');
+  VALUES
+  (
+    'Dan',
+    'danpw'
+  ),
+  (
+    'Carson',
+    'carsonpw'
+  ),
+  (
+    'Tim',
+    'timpw'
+  ),
+  (
+    'Jon',
+    'jonpw'
+  );
+
 
 INSERT INTO blogs(title,post,user_id)
-  VALUES ('You want Crypto?', 'Well you too bad', 1);
+  VALUES
+  (
+    'You want Crypto?',
+    'Well you too bad',
+    1
+  ),
+  (
+    'You got crypto?',
+    'Your life or crypto? Pick one',
+    2
+  ),
+  (
+    'Want to buy bitcoin? Read this',
+    'Buy Theorem',
+    3
+  ),
+  (
+    'Interested in cryptocurrency?',
+    'Ask me',
+    4
+  );
 
