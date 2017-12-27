8430b00eab8bd596a7b97971bff62c76db27b726
Carson Chen <caiyongchen@live.com>
2017-12-27 17:57:16 -0500

    changed in config file with pgpromise

--- dbConfig/config.js
+++ dbConfig/config.js
@@ -1,22 +1,29 @@
-const options = {
-  query: (e) => {
-    console.log(e.query);
-  },
+// const options = {
+//   query: (e) => {
+//     console.log(e.query);
+//   },
+// };
+
+// const pgp = require('pg-promise')(options);
+
+// const config = {
+//   host:     process.env.DB_HOST || 'localhost',
+//   port:     process.env.DB_PORT || 5432,
+//   database: process.env.DB_NAME || 'crypto_blog_dev',
+// };
+
+// function setDatabase() {
+//   return pgp(process.env.DATABASE_URL || config);
+// }
+
+// const db = setDatabase();
+
+// module.exports = db;
+
+// Connecting database and localhost
+
+module.exports = process.env.DATABASE_URL || {
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || 'creddit_users_dev',
 };
-
-const pgp = require('pg-promise')(options);
-
-const config = {
-  host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user:     process.env.DB_USER,
-};
-
-function setDatabase() {
-  return pgp(process.env.DATABASE_URL || config);
-}
-
-const db = setDatabase();
-
-module.exports = db;
