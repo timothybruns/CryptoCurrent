3ea1ed850aa199553ab60dbec997005610116d05
Jonathan Patruno <jpatruno1924@gmail.com>
2018-01-02 14:58:56 -0500

    CSS Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../App.js';
 const Header  = props => {
 	return (
       <div className="header">
-       <h1 className="blogWelcome"> Welcome to the Crypto Blog </h1>
+       <h1 className="blogWelcome"> CryptoCurrent</h1>
        </div>
 		);
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,8 @@ import { Link } from 'react-router-dom';
 export const Home = ({ blogs }) => {
   return blogs !== null ? (
     <section className="blogTitle">
-      <u><p className="blogList"> Blog Index</p></u>
+    
+      <p className="blogList"> Blog List</p>
 
       {/* map through blogs and render all titles */}
 
