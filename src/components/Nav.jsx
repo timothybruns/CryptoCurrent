4aef1891c8c2b1b0c8a92ac061d7d0d614f7eb5f
Daniel Keenan <danfk92@gmail.com>
2018-01-02 09:41:51 -0500

    Css changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Nav  = () => {
   return (
-    
+
       <ul className="navbarInner">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/resources">Resources</Link></li>
-        <li id="createTab"><Link to="/create">Create Blog</Link></li>
+        <a>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/resources">Resources</Link></li>
+          <li id="createTab"><Link to="/create">Create Blog</Link></li>
+        </a>
       </ul>
-    
+
   );
 };
 
